fdabb4a1b2aceaccb858c6a14f8086c65e324e80
jyl6932 <43695939+jyl6932@users.noreply.github.com>
2021-07-06 18:01:29 +0900

    Delete saveFile.m

--- saveFile.m
+++
@@ -1,8 +0,0 @@
-function saveFile(imageFrame,savePath,fileName)
-%UNTITLED2 此处显示有关此函数的摘要
-%   此处显示详细说明
-savePathRoad = [savePath,'\',fileName,'.jpg'];
-saveas(imageFrame,savePathRoad);
-disp("Save the "+fileName+'!!!');
-end
-
