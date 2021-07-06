c8fcf5942afff63d5652f8b69ce32c7022d999be
jyl6932 <43695939+jyl6932@users.noreply.github.com>
2021-07-06 18:01:05 +0900

    Delete func.m

--- func.m
+++
@@ -1,7 +0,0 @@
-function [outputArg1,outputArg2] = func(inputArg1,inputArg2)
-%UNTITLED3 此处显示有关此函数的摘要
-%   此处显示详细说明
-outputArg1 = 1/(sqrt(1+(inputArg2/inputArg1)^2));
-outputArg2 = inputArg2/inputArg1*outputArg1 ;
-return 
-
